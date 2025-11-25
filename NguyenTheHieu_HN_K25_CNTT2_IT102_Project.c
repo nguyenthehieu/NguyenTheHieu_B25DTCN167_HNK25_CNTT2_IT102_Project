624560d98bab01181e8d621043e7c6532747e86f
Hiếu <thehieu090107@gmail.com>
2025-11-25 08:56:00 +0700

    Add files via upload

---
+++ NguyenTheHieu_HN_K25_CNTT2_IT102_Project.c
@@ -0,0 +1,286 @@
+#include<stdio.h>
+#include<string.h>
+#define MAX_Products 100
+#define MAX_Transaction 200
+
+//nguyen mau ham
+void addProduct();
+void updateProduct();
+void managementStatus();
+void searchProduct();
+
+int isEmpty(char str[]);
+int isDuplicateId(char id[]); 
+
+struct Product{
+	char productId[10];   // ma hang hoa
+	char name[50];     // ten hang hoa
+	char unit[10];   // don vi
+	int qty;      // so luong ton kho
+	int status;   //trang thai(1:còn sd, 0: het han)
+};
+
+struct Transaction{ //Xuat/nhap hang
+	char transId[20]; // Ma giao dich (Sinh ngau nhien hoac tu tang).
+	char productId[10]; // Ma hang hoa
+	char type[5];  //Kieu xuat/nhap (IN = nhap/ OUT = xuat).
+	char date[15]; // Thoi gian giao dich.
+};
+
+struct Product prd[MAX_Products];
+struct Transaction trans[MAX_Transaction];
+int productCount = 0; // dem so hang hoa
+int transCount = 0;
+
+//kiem tra du lieu
+int isEmpty(char str[]) { // kiem tra chuoi rong
+    return strlen(str) == 0;
+}
+
+int isDuplicateId(char id[]) { // kiem tra trung nhau
+    for (int i = 0; i < productCount; i++) {
+        if (strcmp(prd[i].productId, id) == 0)
+            return 1;
+    }
+    return 0;
+}
+
+//1. THEM SAN PHAM
+void addProduct() {
+    int amount;
+	printf("<<----- THEM SAN PHAM ----->>\n");
+    printf("Nhap so luong san pham muon them: ");
+    scanf("%d", &amount);
+    getchar();
+
+    if (productCount + amount > MAX_Products) {
+        printf("Kho da day. Khong the them.\n");
+        return;
+    }
+
+    for (int i = 0; i < amount; i++) {
+        printf("\n<<--- San pham %d --->>\n", productCount + 1);
+
+        do {
+            printf("Nhap ma san pham: ");
+            fgets(prd[productCount].productId, 10, stdin);
+            prd[productCount].productId[strcspn(prd[productCount].productId, "\n")] = '\0';
+
+            if (isEmpty(prd[productCount].productId))
+                printf("ID khong duoc trong\n");
+            else if (isDuplicateId(prd[productCount].productId))
+                printf("ID da ton tai, nhap ID khac!\n");
+            else break;
+
+        } while (1);
+
+        do {
+            printf("Nhap ten san pham: ");
+            fgets(prd[productCount].name, 50, stdin);
+            prd[productCount].name[strcspn(prd[productCount].name, "\n")] = '\0';
+
+            if (isEmpty(prd[productCount].name))
+                printf("Ten khong duoc de trong!\n");
+
+        } while (isEmpty(prd[productCount].name));
+        
+        do {
+            printf("Nhap don vi tinh: ");
+            fgets(prd[productCount].unit, 10, stdin);
+            prd[productCount].unit[strcspn(prd[productCount].unit, "\n")] = '\0';
+
+            if (isEmpty(prd[productCount].unit))
+                printf("Don vi tinh khong duoc de trong!\n");
+
+        } while (isEmpty(prd[productCount].unit));
+
+        do {
+            printf("Nhap so luong ton kho: ");
+            scanf("%d", &prd[productCount].qty);
+            getchar();
+            
+            if (prd[productCount].qty < 0)
+                printf("So luong phai >= 0!\n");
+
+        } while (prd[productCount].qty < 0);
+
+        printf("Trang thai (1: con sd, 0: het han): ");
+        scanf("%d", &prd[productCount].status);
+        getchar();
+
+        productCount++;
+    }
+
+    printf("<<--- Them san pham thanh cong. --->>\n");
+}
+
+//2. CAP NHAT SAN PHAM
+void updateProduct() {
+	char id[10];
+	printf("<<----- CAP NHAT SAN PHAM ----->>\n");
+	printf("Nhap ma san pham can cap nhat: ");
+	fgets(id,10,stdin);
+	id[strcspn(id,"\n")]='\0';
+	
+	int found = -1;
+	
+	for(int i=0; i<productCount; i++){    //tim ID
+		if( strcmp(id, prd[i].productId) == 0){
+			found = i;
+			break;
+		}
+	}	
+	if (found == -1){
+		printf("Khong tim thay.\n");
+		return;
+	}
+	printf("<<--- CAP NHAT SAN PHAM --->>\n");
+	
+	
+    do{
+		printf("Nhap ten moi: ");
+		char newName[50];
+		fgets(newName, 50, stdin);
+		newName[strcspn(newName, "\n")] = '\0';
+	
+		if(!isEmpty(newName)){
+			strcpy(prd[found].name, newName);
+	 		break;
+	 	} else{
+        	printf("Ten khong duoc de trong!\n");
+		}
+    } while(1);
+	
+	do{
+		printf("Nhap don vi moi: ");
+		char newUnit[10];
+		fgets(newUnit, 10, stdin);
+		newUnit[strcspn(newUnit, "\n")] ='\0';
+		
+		if(!isEmpty(newUnit)){
+			strcpy(prd[found].unit, newUnit);
+			break;
+		} else {
+        	printf("Don vi tinh khong duoc de trong!\n");	
+		}	
+	} while(1);
+	
+	do{
+		printf("Nhap so luong moi: ");
+		int newQty;
+		scanf("%d",&newQty);
+		getchar();
+		if(newQty>=0){
+			prd[found].qty = newQty;
+			break;
+		} else{
+        	printf("So luong phai >= 0!\n");
+        }
+    
+	} while(1);
+	
+	printf("<<---Cap nhat thanh cong--->>\n");
+	}
+
+//3. QUAN LY TRANG THAI
+void managementStatus(){
+	char id[10];
+	printf("<<----- QUAN LY TRANG THAI ----->>\n");
+	printf("Nhap ma san pham can doi trang thai: ");
+	fgets(id,10,stdin);
+	id[strcspn(id,"\n")]='\0';
+	
+	int found = -1;
+	
+	for(int i=0; i<productCount; i++){    //tim ID
+		if( strcmp(id, prd[i].productId) == 0){
+			found = i;
+			break;
+		}
+	}	
+	if (found == -1){
+		printf("Khong ton tai trong danh sach.\n");
+		return;
+	}
+	printf("Nhap trang thai moi (1-Con su dung, 0-Het han): ");
+	int newStatus;
+	scanf("%d",&newStatus);
+	getchar();
+	prd[found].status = newStatus;
+	
+	printf("<<---Cap nhat hang hoa thanh cong--->>\n");
+}
+
+//4. TIM KIEM HANG HOA
+void searchProduct(){
+	char key[50];
+	printf("<<----- TIM KIEM HANG HOA ----->>\n");
+	printf("Nhap ma hoac ten san pham can tim: ");
+	fgets(key,50,stdin);
+	key[strcspn(key,"\n")]='\0';
+	
+	int found = 0;
+	printf("\n<<--- Thong tin san pham --->>\n");
+	
+	for(int i=0; i<productCount; i++){    
+		if( strcmp(key, prd[i].productId) == 0 || strstr(key, prd[i].name) != NULL){
+			printf("| %s | %s | %s | %d | %s |\n", prd[i].productId, prd[i].name, 
+			prd[i].unit, prd[i].qty, prd[i].status == 1 ? "Con su dung" : "Het han");
+			found = 1;
+		} 
+	}
+	if(!found)
+		printf("Khong tim thay thong tin nao.\n");	
+	}
+
+
+int main (){
+	int choice;
+
+	while(1){
+	printf("\n+----------QUAN LY HANG HOA----------+\n");
+	printf("1. Them ma hang moi.     	 	    \n");
+	printf("2. Cap nhat thong tin.              \n");
+	printf("3. Quan ly trang thai.   \n");
+	printf("4. Tra cuu san pham.                \n");
+	printf("5. Dang sach (Phan trang).          \n");
+	printf("6. Sap xep danh sach.              \n");
+	printf("7. Giao dich xuat/nhap hang hoa.    \n");
+	printf("8. Lich su xuat/nhap.               \n");
+	printf("+------------------------------------+\n");
+
+	printf("\nMoi ban nhap lua chon: ");
+	scanf("%d",&choice);
+
+	switch(choice){
+		case 1:
+			addProduct();
+			break;
+		case 2:
+			getchar();
+			updateProduct();
+			break;
+		case 3:
+			getchar();
+			managementStatus();
+			break;
+		case 4:
+			getchar();
+			searchProduct();
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+		case 7:
+			break;
+		case 8:
+			break;
+
+		default:
+			printf("Lua chon khong hop le. \n");
+			return 0;
+	}
+	}
+	return 0;
+}
